ba7934dc3bf3511e752f6df87922daebf76bb3a6
Yahikko <taczuhiro@mail.ru>
2024-09-06 08:53:15 +0300

    algs

---
+++ easy/219_ContainsDuplicate2/s1.ts
@@ -0,0 +1,12 @@
+const containsNearbyDuplicate = (nums: number[], k: number): boolean => {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i - map[nums[i]] <= k) {
+      return true;
+    }
+    map[nums[i]] = i
+  }
+
+  return false;
+};
